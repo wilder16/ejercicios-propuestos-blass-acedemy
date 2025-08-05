f7032efd66aab67f7f7f553fffced1fdabb88222
Wilder de Jesus Bernal Lopez <wjblah@gmail.com>
2025-08-05 12:35:25 -0500

    ejercicios mostrar datos por consola

--- TareasCap2/src/declaracionconstantes/Ejercicio1.java
+++ TareasCap2/src/declaracionconstantes/Ejercicio1.java
@@ -4,12 +4,16 @@ public class Ejercicio1 {
     public static void main(String[] args) {
 
         final var nombre = "Jorge";
-        final var primerApellido = "Sifuentes";
+        final var apellido = "Sifuentes";
         final var segundoApellido = "Caceres";
         final var edad = 52;
         final var peso = 25.12;
         final var esTitulado = false;
         final var aula = "A";
+
+        System.out.printf("Mi nombre es %s y mi apellido es %s%n", nombre, apellido);
+        System.out.printf("Tengo %d años de edad y peso %.2fkg%n", edad, peso);
+        System.out.printf("Soy titulado? %b, aun estudio en el aula con sección %s", esTitulado, aula);
 
     }
 
--- TareasCap2/src/declaracionconstantes/Ejercicio2.java
+++ TareasCap2/src/declaracionconstantes/Ejercicio2.java
@@ -8,5 +8,11 @@ public class Ejercicio2 {
         final var sonAmericanos = false;
         final var cantidadAlbumes = 19;
         final var rating = 4.85;
+
+        System.out.printf("Mi banda favorita es %s con %d albums%n", banda, cantidadAlbumes);
+        System.out.printf("Sus integrantes conocidos son el cantente llamado %s y el guitarrista " +
+                "con el nombre %s%n", vocalista, guitarrista);
+        System.out.printf("Son de nacionalidad estadounidense? %b, pero igual tiene un rating de %.2f",
+                sonAmericanos, rating);
     }
 }
