11c51f6a714b9f2f1a616ccfb14a8f21d7a8e25b
Wilder de Jesus Bernal Lopez <wjblah@gmail.com>
2025-08-05 11:50:08 -0500

    ejercicios claracion de constantes

---
+++ TareasCap2/src/declaracionconstantes/Ejercicio1.java
@@ -0,0 +1,16 @@
+package declaracionconstantes;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        final var nombre = "Jorge";
+        final var primerApellido = "Sifuentes";
+        final var segundoApellido = "Caceres";
+        final var edad = 52;
+        final var peso = 25.12;
+        final var esTitulado = false;
+        final var aula = "A";
+
+    }
+
+}
---
+++ TareasCap2/src/declaracionconstantes/Ejercicio2.java
@@ -0,0 +1,12 @@
+package declaracionconstantes;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        final var banda = "Black Sabbath";
+        final var guitarrista = "Tony Lommi";
+        final var vocalista = "Ozzy";
+        final var sonAmericanos = false;
+        final var cantidadAlbumes = 19;
+        final var rating = 4.85;
+    }
+}
